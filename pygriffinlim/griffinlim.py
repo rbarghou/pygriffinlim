c80be5b52163986863ee664b227289713e935aeb
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-11 09:46:17 -0400

    Added some basic griffin lim implementation

--- pygriffinlim/griffinlim.py
+++ pygriffinlim/griffinlim.py
@@ -1,13 +1,76 @@
 import librosa
+import numpy as np
 
 
-def griffin_lim_reconstruction(spectrogram, iterations):
+DEFAULT_STFT_KWARGS = {
+    "hop_length": 512,
+    "n_fft": 2048,
+}
+
+
+def griffin_lim_generator(
+        spectrogram,
+        iterations=10,
+        approximated_signal=None,
+        stft_kwargs=DEFAULT_STFT_KWARGS):
+    """
+    Implements the basic Griffin Lim algorithm.
+
+    Returns a generator that outputs the approximated signal at the various iterations.
+    :param spectrogram: The Spectrogram from which reconstruction should begin
+    :param iterations: The number of iterations you want to perform reconstruction with.
+    :param approximated_signal: if you want to begin with an existing approximated signal
+    :param stft_kwargs: The arguments to pass to STFT and ISTFT as defined by the librosa
+    implementation of these functions.
+    :return generator:  This is a generator of approximated signals at each iteration of
+    the griffin lim algorithm
+    """
+    _M = spectrogram
+    for k in range(iterations):
+        if approximated_signal is None:
+            _P = np.random.randn(*_M.shape)
+        else:
+            _D = librosa.stft(approximated_signal, **stft_kwargs)
+            _P = np.angle(_D)
+
+        _D = _M * np.exp(1j * _P)
+        approximated_signal = librosa.istft(_D, **stft_kwargs)
+        yield approximated_signal
+
+
+def gla(spectrogram,
+        iterations=10,
+        approximated_signal=None,
+        stft_kwargs=DEFAULT_STFT_KWARGS):
+    """
+    Implements the basic Griffin Lim algorithm.
+
+    Returns a generator that outputs the approximated signal at the various iterations.
+    :param spectrogram: The Spectrogram from which reconstruction should begin
+    :param iterations: The number of iterations you want to perform reconstruction with.
+    :param approximated_signal: if you want to begin with an existing approximated signal
+    :param stft_kwargs: The arguments to pass to STFT and ISTFT as defined by the librosa
+    implementation of these functions.
+    :return approximated_signal:
+    """
+    generator = griffin_lim_generator(spectrogram, iterations, approximated_signal, stft_kwargs)
+    for approximated_signal in generator:
+        pass
+    return approximated_signal
+
+
+def fast_griffin_lims_generator(
+        spectrogram,
+        iterations=10,
+        alpha=.1,
+        stft_kwargs=DEFAULT_STFT_KWARGS):
     pass
 
 
-def fast_griffin_lims_reconstruction(spectrogram, iterations, alpha):
-    pass
-
-
-def modified_fast_griffin_lim_reconstruction(spectrogram, iterations, alpha_loc, alpha_scale):
+def modified_fast_griffin_lim_generator(
+        spectrogram,
+        iterations,
+        alpha_loc=.1,
+        alpha_scale=.4,
+        stft_kwargs=DEFAULT_STFT_KWARGS ):
     pass
