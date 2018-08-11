a00c2845b9e6bf0c115f1cfbde594464dba1e405
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-11 09:03:30 -0400

    Added super basic setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pygriffinlim',
+      version='0.1',
+      description='An implementation of the Griffin Lim Algorithm',
+      author='Ramsey Barghouti',
+      license='GPL3',
+      packages=['pygriffinlim'],
+      zip_safe=False)
